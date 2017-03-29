0cff6087b611bd46a72796c8bd3374cf61f94523
YuriiKrat <yuriikrat97@gmail.com>
2017-03-29 20:00:04 +0300

    refactoring

--- src/main/java/dao/db/projects/ProjectDAO.java
+++ src/main/java/dao/db/projects/ProjectDAO.java
@@ -1,19 +1,12 @@
 package dao.db.projects;
 
 import dao.db.AbstractDAOImpl;
-import dao.db.DataType;
 import entities.project.Project;
-import entities.project.Sprint;
-import javafx.util.Pair;
 import org.apache.log4j.Logger;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Yurii Krat
@@ -45,53 +38,7 @@ public class ProjectDAO extends AbstractDAOImpl<Project, Integer> {
                 }
                 resultSet.beforeFirst();
             }
-            while (resultSet.next()) {
-                Integer attribute_id = resultSet.getInt("id");
-                String attribute_name = resultSet.getString("name");
-                String type_name = resultSet.getString("type_name");
-                Pair<String, Class> pair = getTypeInfo(type_name);
-                String column = pair.getKey();
-                Class classType = pair.getValue();
-                if (column == null) {
-                    column = entityIdColumn;
-                }
-                if (classType == null) {
-                    classType = List.class;
-                }
-                String getValueQuery = "SELECT " + column + " FROM " + valuesTableName +
-                        " WHERE attribute_id = ?;";
-                PreparedStatement getValueStatement =
-                        connection.prepareStatement(getValueQuery);
-                getValueStatement.setInt(1, attribute_id);
-                ResultSet value = getValueStatement.executeQuery();
-                if (value.next()) {
-                    Class clazz = project.getClass();
-                    try {
-                        Method method = clazz.getMethod("set" + attribute_name, classType);
-                        if (DataType.INTEGER.toString().equals(type_name)) {
-                           method.invoke(project, value.getInt(integerValueColumn));
-                        } else if (DataType.STRING.toString().equals(type_name)) {
-                            method.invoke(project, value.getString(textValueColumn));
-                        } else if (DataType.BOOLEAN.toString().equals(type_name)) {
-                            method.invoke(project, value.getBoolean(booleanValueColumn));
-                        } else if (DataType.DATE.toString().equals(type_name)) {
-                            method.invoke(project, value.getDate(dateValueColumn));
-                        } else if (DataType.DOUBLE.toString().equals(type_name)) {
-                            method.invoke(project, value.getDouble(doubleValueColumn));
-                        } else {
-                            List<Sprint> sprints = new ArrayList<>();
-                            do {
-                                sprints.add(sprintDAO.get(value.getInt(entityIdColumn)));
-                            } while (value.next());
-                            method.invoke(project, sprints);
-                        }
-
-                    } catch (IllegalAccessException | InvocationTargetException |
-                            NoSuchMethodException e) {
-                        logger.error("Failed to set value! " + e.getMessage());
-                    }
-                }
-            }
+            setFields(resultSet, project, sprintDAO);
 
         } catch (SQLException e) {
             logger.error("Failed to retrieve sprint from db! " + e.getMessage());
